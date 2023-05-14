78f703ebb22a35e044a1649c01695a939fdd720a
Minato <umi0n3@gmail.com>
2023-05-14 16:46:51 +0200

    producer exception

--- SendMailApi/Controllers/MailController.cs
+++ SendMailApi/Controllers/MailController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Mvc;
 using RabbitMqShared.Models;
 using RabbitMqShared.Services;
 
@@ -9,17 +10,26 @@ namespace SendMailApi.Controllers;
 public class MailController : ControllerBase
 {
     private readonly IProducer _producer;
+    private readonly ILogger _logger;
 
-    public MailController(IProducer producer)
+    public MailController(IProducer producer, ILogger logger)
     {
         _producer = producer;
+        _logger = logger;
     }
 
     [Route("[action]")]
     [HttpPost]
     public IActionResult SendMessage(MailBody mailBody)
     {
-        _producer.SendMessage(mailBody);
+        try
+        {
+            _producer.SendMessage(mailBody);
+        }
+        catch (Exception e)
+        {
+            _logger.LogInformation("Producer failed: {Error}", e.Message);
+        }
         
         return Ok();
     }
