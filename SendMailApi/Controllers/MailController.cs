4167d623fe3ea098613eaf6aaa3dfd0b8a69121a
Minato <umi0n3@gmail.com>
2023-05-14 16:23:01 +0200

    send mail api

---
+++ SendMailApi/Controllers/MailController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMqShared.Models;
+using RabbitMqShared.Services;
+
+namespace SendMailApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MailController : ControllerBase
+{
+    private readonly IProducer _producer;
+
+    public MailController(IProducer producer)
+    {
+        _producer = producer;
+    }
+
+    [Route("[action]")]
+    [HttpPost]
+    public IActionResult SendMessage(MailBody mailBody)
+    {
+        _producer.SendMessage(mailBody);
+        
+        return Ok();
+    }
+}
